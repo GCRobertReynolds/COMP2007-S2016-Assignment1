8511a41321fed69f2b13985d963ad768b5a42b00
Robert Reynolds <gcrobertreynolds@gmail.com>
2016-06-01 20:59:03 -0400

    added addActiveClass method to Site.Master.Cs

--- Comp2007-Assignment1/Default.aspx
+++ Comp2007-Assignment1/Default.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Comp2007_Assignment1.Default" %>
+﻿<%@ Page Title="Home" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Comp2007_Assignment1.Default" %>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
     <div class="container">
--- Comp2007-Assignment1/Site.Master.cs
+++ Comp2007-Assignment1/Site.Master.cs
@@ -11,7 +11,36 @@ namespace Comp2007_Assignment1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            addActiveClass();
+        }
 
+        /**
+         * This method adds the active class to each <li> in the main navbar
+         * according to the page title
+         * 
+         * @method addActiveClass
+         */
+        private void addActiveClass()
+        {
+            switch (Page.Title)
+            {
+                case "Home":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Products":
+                    products.Attributes.Add("class", "active");
+                    break;
+                case "Services":
+                    services.Attributes.Add("class", "active");
+                    break;
+                case "About":
+                    about.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+
+            }
         }
     }
 }
